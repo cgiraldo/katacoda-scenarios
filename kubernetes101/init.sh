fa53ba9b7a6bfda52aca80f34abe368548bd3916
Carlos Giraldo <cgiraldo@gradiant.org>
2018-09-06 14:31:32 +0200

    commit

--- kubernetes101/init.sh
+++ kubernetes101/init.sh
@@ -6,19 +6,4 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minik
 sudo mv minikube /usr/local/bin/
 minikube start --vm-driver=none
 
-
-cat <<EOF | kubectl create -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: dashboard
-spec:
-  selector:
-    app: kubernetes-dashboard
-  type: NodePort
-  ports:
-  - nodePort: 30000
-    port: 80
-    protocol: TCP
-    targetPort: 9090
-EOF
+minikube dashboard
