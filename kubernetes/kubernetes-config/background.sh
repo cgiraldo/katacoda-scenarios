8a3e1a22526f28f3d0f8eede3d2bbe17c972aefe
Carlos Giraldo <cgiraldo@gradiant.org>
2020-05-21 14:44:03 +0200

    commit

--- kubernetes/kubernetes-config/background.sh
+++ kubernetes/kubernetes-config/background.sh
@@ -11,7 +11,4 @@ wget https://kubernetes.io/examples/configmap/ui.properties -O game-config-files
 wget https://kubernetes.io/examples/pods/pod-configmap-envFrom.yaml -O manifests/pod-configmap-envFrom.yaml
 wget https://kubernetes.io/examples/configmap/configmap-multikeys.yaml -O manifests/configmap-multikeys.yaml
 
-wget https://kubernetes.io/examples/pods/inject/secret.yaml -O manifests/secret.yaml
-wget https://kubernetes.io/examples/pods/inject/secret-pod.yaml -O manifests/secret-pod.yaml
 
-wget https://kubernetes.io/examples/pods/pod-configmap-volume.yaml -O manifests/pod-configmap-volume.yaml
